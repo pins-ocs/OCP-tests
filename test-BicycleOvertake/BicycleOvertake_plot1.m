66000426e8a6732dcf91505b7349a0fa5c65da12
Enrico Bertolazzi <enrico.bertolazzi@unitn.it>
2016-09-14 19:25:13 +0200

    Aggiunto Bicycle overtake

---
+++ test-BicycleOvertake/BicycleOvertake_plot1.m
@@ -0,0 +1,32 @@
+%data = importdata('data/BicycleOvertake_out.txt') ;
+data = importdata('data/BicycleOvertake_not_converged.txt') ;
+
+
+zeta = data.data(:,find( strcmp(data.textdata,'zeta') )) ;
+TimeSize = data.data(:,find( strcmp(data.textdata,'TimeSize') )) ;
+t    = zeta.*TimeSize ;
+s1   = data.data(:,find( strcmp(data.textdata,'s1') )) ;
+v1   = data.data(:,find( strcmp(data.textdata,'v1') )) ;
+s2   = data.data(:,find( strcmp(data.textdata,'s2') )) ;
+v2   = data.data(:,find( strcmp(data.textdata,'v2') )) ;
+u    = data.data(:,find( strcmp(data.textdata,'u') )) ;
+ds   = data.data(:,find( strcmp(data.textdata,'ds') )) ;
+scia = data.data(:,find( strcmp(data.textdata,'scia') )) ;
+h1   = data.data(:,find( strcmp(data.textdata,'h1') )) ;
+h2   = data.data(:,find( strcmp(data.textdata,'h2') )) ;
+
+subplot(4,1,1) ;
+plot( t, s1-s2 ) ; %, t, ds ) ;
+title('s1-s2') ;
+
+subplot(4,1,2) ;
+plot( t, u ) ;
+title('u') ;
+
+subplot(4,1,3) ;
+plot( t, v1, t, v2 ) ;
+title('v1, v2') ;
+
+subplot(4,1,4) ;
+plot( t, scia ) ;
+title('step') ;
