fca2311237e26794f61c474a74fe33ff11d68e57
Enrico Bertolazzi <enrico.bertolazzi@unitn.it>
2023-02-03 16:58:27 +0100

    corretto BORZI

--- test-Brake/MATLAB/Brake_DIRECT_sys2.m
+++ test-Brake/MATLAB/Brake_DIRECT_sys2.m
@@ -2,10 +2,8 @@ global N epsilon;
 
 close all;
 
-N       = 200;
-epsilon = 0.000000001;
-
-[lb,ub] = bounds();
+N       = 50;
+epsilon = 0.1e-4;
 
 opts = optimoptions(...
   'lsqnonlin', ...
@@ -27,15 +25,20 @@ fprintf('exitflag = %d\n',exitflag);
 fprintf('iterations = %d\n',output.iterations);
 fprintf('%s\n',output.message);
 
-[ X, V, L1, L2, T ] = unpack( Z );
+[ X, V, L1, L2, A, T ] = unpack( Z );
 
-ACC = get_ACC( (L2(1:end-1)+L2(2:end))./2, T );
+L2M  = (L2(2:end)+L2(1:end-1))./2;
+ACC = get_ACC( L2M, T );
 
-AA = reshape( [ACC.';ACC.'], 1, 2*N );
-XX = reshape( [X(1:end-1).';X(2:end).'], 1, 2*N );
+AA  = reshape( [A.';A.'], 1, 2*N );
+AAA = reshape( [ACC.';ACC.'], 1, 2*N );
+XX  = reshape( [X(1:end-1).';X(2:end).'], 1, 2*N );
 
 subplot(3,1,1);
+hold off;
 plot( XX, AA, 'LineWidth', 3 );
+hold on;
+plot( XX, AAA, 'LineWidth', 3 );
 xlim([X(1),X(end)]);
 ylim([-1.1,1.1]);
 title('acceleration');
@@ -64,21 +67,9 @@ function Z = guess()
   V  = ones(m,1);
   L1 = ones(m,1);
   L2 = 1-X;
+  A  = zeros(N,1);
   T  = 1;
-  Z  = [X;V;L1;L2;T];
-end
-%
-% ========================================================================
-%
-function [L,U] = bounds()
-  global N;
-  m    = N+1;
-  Xmin = zeros(m,1); Xmax = 10*ones(m,1);
-  Vmin = zeros(m,1); Vmax = 10*ones(m,1);
-  Amin = -0.9999*ones(N,1); Amax = 0.9999*ones(N,1);
-  Tmin = 0.001;      Tmax = 10;
-  L = [ Xmin; Vmin; -Inf*ones(2*m,1); Tmin ]-0.0001;
-  U = [ Xmax; Vmax;  Inf*ones(2*m,1); Tmax ];
+  Z  = [X;V;L1;L2;A;T];
 end
 %
 % ========================================================================
@@ -91,7 +82,7 @@ end
 %
 % ========================================================================
 %
-function F = fd_sys( X, V, L1, L2, T )
+function F = fd_sys( X, V, L1, L2, A, T )
   global N epsilon;
   h    = 1/N;
   VM   = (V(2:end)+V(1:end-1))./2;
@@ -105,32 +96,33 @@ function F = fd_sys( X, V, L1, L2, T )
   ACC = get_ACC( L2M, T );
 
   FD1 = X_D - T*VM;
-  FD2 = V_D - T*ACC;
+  FD2 = V_D - T*A;
   FD3 = L1_D;
   FD4 = L2_D+T*L1M;
   %  VA SCALATA!
-  FD5 = sum(h*(L1M.*VM + L2M.*ACC -epsilon*log( cos(pi/2*ACC) ) ));
-  BC  = [ X(1); V(1)-1; V(end); 1-L1(end) ];
+  FD5 = sum(h*(L1M.*VM + L2M.*A))./h;% - D(Tpositive)(-T))
+  BC  = [ X(1); V(1)-1; V(end); L2(end) ];
   W   = 1;
-  F   = [ W*FD1; W*FD2; FD3; FD4; FD5; BC ];
+  F   = [ W*FD1; W*FD2; FD3; FD4; FD5; BC; A - ACC ];
 end
 %
 % ========================================================================
 %
-function [ X, V, L1, L2, T ] = unpack( Z )
+function [ X, V, L1, L2, A, T ] = unpack( Z )
   global N;
   m   = N+1;
   X   = Z(0*m+1:1*m);
   V   = Z(1*m+1:2*m);
   L1  = Z(2*m+1:3*m);
   L2  = Z(3*m+1:4*m);
-  T   = Z(4*m+1);
+  A   = Z(4*m+1:5*m-1);
+  T   = Z(5*m);
 end
 %
 % ========================================================================
 %
 function F = sys( Z )
   global epsilon;
-  [ X, V, L1, L2, T ] = unpack( Z );
-  F = fd_sys( X, V, L1, L2, T );
+  [ X, V, L1, L2, A, T ] = unpack( Z );
+  F = fd_sys( X, V, L1, L2, A, T );
 end
